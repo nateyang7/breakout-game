aaae881f901c4b827250707306a7d8bcd3088dd7
Nathan Yang <nathanyang.w@outlook.com>
2026-04-01 01:42:20 +0200

    Add button to HTML and ball movements in JS

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+const runButton = document.getElementById('runButton');
+runButton.addEventListener('click', () => {
+  startGame();
+  runButton.disabled = true;
+});
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50); // x, y, width, height
-ctx.fillStyle = 'red';
-ctx.fill();
-ctx.closePath();
+// Ball position and movements
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+const dx = 1;
+const dy = -1;
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = 'green';
-ctx.fill();
-ctx.closePath();
+/**
+ * Draw a ball.
+ */
+const drawBall = () => {
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
+}
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = 'rgb(0 0 255 / 0.5)';
-ctx.stroke();
-ctx.closePath();
+/**
+ * Draw the game.
+ */
+const draw = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  x += dx;
+  y += dy;
+};
+
+/**
+ * Start the game.
+ */
+const startGame = () => {
+  setInterval(draw, 10)
+};
