ee0a91b2216190ee1ef9f4268422fb87d4bc45f6
Nathan Yang <nathanyang.w@outlook.com>
2026-04-04 14:24:05 +0200

    Configure canvas scaling + background color

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ const config = {
   width: 480,
   height: 320,
   scene: ExampleScene,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  backgroundColor: "#eeeeee",
 };
 
 const game = new Phaser.Game(config);
