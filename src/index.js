7b1c4561ce1a7f5f29f61ed3519a714da5afc2d4
Nathan Yang <nathanyang.w@outlook.com>
2026-04-04 18:28:33 +0200

    Add random ball velocity for bounce when hit by paddle

--- src/index.js
+++ src/index.js
@@ -207,6 +207,7 @@ class ExampleScene extends Phaser.Scene {
 
   hitPaddle(ball, paddle) {
     this.ball.anims.play("wobble");
+    ball.body.velocity.x = -5 * (paddle.x - ball.x);
   }
 
   ballLeaveScreen() {
