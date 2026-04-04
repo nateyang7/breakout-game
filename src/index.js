0325fb649b303392292b617710902b20844396f9
Nathan Yang <nathanyang.w@outlook.com>
2026-04-04 15:56:03 +0200

    Add immovable property to paddle + Collision detection between paddle and ball

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 class ExampleScene extends Phaser.Scene {
   ball;
+  paddle;
   preload() {
     this.load.image("ball", "img/ball.png");
+    this.load.image("paddle", "img/paddle.png");
   }
   create() {
+    // Ball
     this.ball = this.add.sprite(50, 50, "ball");
     this.physics.add.existing(this.ball);
-    this.ball.body.setVelocity(150, 150);
+    this.ball.body.setVelocity(500, 500);
     this.ball.body.gravity.y;
     this.ball.body.setCollideWorldBounds(true, 1, 1);
+    this.ball.body.setBounce(1);
+
+    // Paddle
+    this.paddle = this.add.sprite(
+      this.scale.width * 0.5,
+      this.scale.height - 5,
+      "paddle",
+    );
+    this.paddle.setOrigin(0.5, 1);
+    this.physics.add.existing(this.paddle);
+    this.paddle.body.setImmovable();
   }
-  update() { }
+  update() {
+    this.physics.collide(this.ball, this.paddle);
+  }
 }
 
 const config = {
